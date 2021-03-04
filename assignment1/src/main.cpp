0ffe8cb0c61571b5e4dfdd4bac753835fdb506ca
Harsh Agrawal <harsh.ag14901@gmail.com>
2021-03-04 22:54:20 +0530

    Basic queue density estimation - Added background subtraction, noise reduction boiler plate in main.cpp - Create a function to output csv data for queue and dynamic density - Created a plotter script in python3

--- assignment1/src/main.cpp
+++ assignment1/src/main.cpp
@@ -10,18 +10,17 @@
 using namespace cv;
 using namespace std;
 
-Mat input_file, input_file_bnw, input_display;
+VideoCapture cap;
+const auto training_percent = 1.0;
+const auto learning_rate = 0;
+const auto kernel_shape = Size(3, 3);
+const auto fill_color = Scalar(100, 100, 0);
+vector<pair<double, double>> density;
 
-const auto dot_color = Scalar(255, 0, 0), line_color = Scalar(255, 0, 0),
-           fill_color = Scalar(100, 100, 0);
-
-const auto original_name = "original", transformed_name = "transformed",
-           cropped_name = "cropped";
-
-void initialize_images();
 void show_usage(string name);
 bool handle_arguments(int argc, char* argv[]);
-void save_images(Mat& transformed_img, Mat& cropped_img);
+bool initialize_video();
+void outputCSV();
 
 int main(int argc, char* argv[]) {
   if (!handle_arguments(argc, argv)) {
@@ -29,37 +28,96 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
-  initialize_images();
-
-  auto selection_window = SelectionWindow(original_name, input_display,
-                                          {dot_color, line_color, fill_color});
-  selection_window.show();
-
-  if (selection_window.start_points.size() != 4) {
-    cout << "[-] User did not select 4 points, Exiting." << endl;
+  if (!initialize_video()) {
+    cerr << "[-] Unable to open the video" << endl;
     return -1;
   }
 
-  Mat intermediate_img, transformed_img, cropped_img;
+  auto frame_rate = cap.get(CAP_PROP_FPS);
+  auto frame_count = cap.get(CAP_PROP_FRAME_COUNT);
+  auto video_ht = cap.get(CAP_PROP_FRAME_HEIGHT);
+  auto video_wd = cap.get(CAP_PROP_FRAME_WIDTH);
+  auto frame_area = video_ht * video_wd;
 
-  auto animated_window = AnimatedWindow(
-      transformed_name, input_file_bnw, selection_window.start_points,
-      (arg_parser.get_bool_argument_value("no-animation")) ? 1 : 120);
-  animated_window.show();
-  animated_window.get_display(transformed_img);
+  cout << "[+] Frame rate is: " << frame_rate << endl;
+  cout << "[+] Frame count is: " << frame_count << endl;
 
-  crop_end_pts(transformed_img, cropped_img, selection_window.start_points);
+  Mat frame, fg_mask, frame_gray;
+  Mat processed_img;
 
-  Window(cropped_name, cropped_img).show();
+  auto bg_sub = createBackgroundSubtractorMOG2(
+      int(frame_count * training_percent / 100.0));
+  auto kernel = getStructuringElement(MORPH_ELLIPSE, kernel_shape);
 
-  save_images(transformed_img, cropped_img);
+  cout << "[+] Training BG subtractor ..." << endl;
+  for (int i = 0; i < bg_sub->getHistory(); i++) {
+    cap.read(frame);
+    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+
+    bg_sub->apply(frame_gray, fg_mask, learning_rate);
+  }
+
+  while (true) {
+    cap.read(frame);
+    if (frame.empty()) {
+      cerr << "[+] Video stream finished" << endl;
+      break;
+    }
+    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+    bg_sub->apply(frame_gray, fg_mask, learning_rate);
+
+    if (arg_parser.get_bool_argument_value("debug")) {
+      imshow("Preprocessed", fg_mask);
+    }
+    // TODO: Change the number of iterations of the three ops below.
+    morphologyEx(fg_mask, processed_img, MORPH_CLOSE, kernel, Point(-1, -1), 1);
+    morphologyEx(processed_img, fg_mask, MORPH_OPEN, kernel, Point(-1, -1), 2);
+    morphologyEx(fg_mask, processed_img, MORPH_DILATE, kernel, Point(-1, -1),
+                 1);
+
+    threshold(processed_img, fg_mask, 240, 255, THRESH_BINARY);
+
+    vector<vector<Point>> contours;
+    vector<vector<Point>> rects;
+
+    findContours(fg_mask, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+
+    if (arg_parser.get_bool_argument_value("debug")) {
+      for (uint i = 0; i < contours.size(); i++) {
+        Rect bnd_rect = boundingRect(contours[i]);
+        rects.push_back(vector<Point>());
+        rects[i].push_back(Point(bnd_rect.x, bnd_rect.y));
+        rects[i].push_back(Point(bnd_rect.x + bnd_rect.width, bnd_rect.y));
+        rects[i].push_back(
+            Point(bnd_rect.x + bnd_rect.width, bnd_rect.y + bnd_rect.height));
+        rects[i].push_back(Point(bnd_rect.x, bnd_rect.y + bnd_rect.height));
+        drawContours(fg_mask, rects, i, fill_color, 4);
+      }
+    }
+    double area = 0;
+    for (auto& contour : contours) {
+      area += contourArea(contour);
+    }
+    area /= 2.0;
+    density.push_back(make_pair(area / frame_area, 0));
+
+    if (arg_parser.get_bool_argument_value("debug")) {
+      imshow("MOG2", fg_mask);
+      if (waitKey(1) == 'n') {
+        break;
+      }
+    }
+  }
+  cap.release();
+  destroyAllWindows();
+  outputCSV();
 
   return 0;
 }
 
 bool handle_arguments(int argc, char* argv[]) {
-  arg_parser.set_argument("input", "i", "input_files/empty.jpg");
-  arg_parser.set_argument("output", "o", "output_files/");
+  arg_parser.set_argument("input", "i", "./input_files/trafficvideo.mp4");
+  arg_parser.set_argument("output", "o", "./output_files/density.csv");
   arg_parser.set_standalone_argument("autoselect-points", "a");
   arg_parser.set_standalone_argument("debug", "d");
   arg_parser.set_standalone_argument("no-animation", "f");
@@ -67,46 +125,41 @@ bool handle_arguments(int argc, char* argv[]) {
   return arg_parser.parse_arguments(argc, argv);
 }
 void show_usage(string name) {
-  std::cerr
-      << "Usage: " << name << "\n"
-      << "Options:\n"
-      << "\t-h, --help\t\tShow this help message\n"
-      << "\t-i, --input\t\tSpecify the input file path. Default is "
-         "./input_files/empty.jpg\n"
-      << "\t-o, --output\t\tSpecify the output directory path. Default is "
-         "./output_files\n"
-      << "\t-a, --autoselect-points \tSelect second set of points "
-         "automatically\n"
-      << "\t-d, --debug \t\tDisplay debug output\n"
-      << "\t-f, --no-animation \t\tDo not display animation\n"
-      << std::endl;
+  cerr << "Usage: " << name << "\n"
+       << "Options:\n"
+       << "\t-h, --help\t\tShow this help message\n"
+       << "\t-i, --input\t\tSpecify the input file path. Default is "
+          "./input_files/empty.jpg\n"
+       << "\t-o, --output\t\tSpecify the output csv file. Default is "
+          "./output_files/density.csv\n"
+       << "\t-a, --autoselect-points \tSelect second set of points "
+          "automatically\n"
+       << "\t-d, --debug \t\tDisplay debug output\n"
+       << "\t-f, --no-animation \t\tDo not display animation\n"
+       << std::endl;
 }
 
-void initialize_images() {
+bool initialize_video() {
   string file_name = arg_parser.get_argument_value("input");
   cout << "[+] Loading File: " << file_name << endl;
+  cap.open(file_name);
 
-  input_file = imread(file_name, IMREAD_COLOR);
-  cvtColor(input_file, input_file_bnw, COLOR_BGR2GRAY);
-  cvtColor(input_file, input_display, COLOR_BGR2BGRA);
+  return cap.isOpened();
 }
 
-void save_images(Mat& transformed_img, Mat& cropped_img) {
-  auto output_dir = arg_parser.get_argument_value("output");
+void outputCSV() {
+  string file_name = arg_parser.get_argument_value("output");
+  fstream out_file;
+  out_file.open(file_name, ios::out);
+  if (!out_file) {
+    cerr << "[-] Error writing to the output file: " << file_name << endl;
+  } else {
+    out_file << "frame_num,queue_density,dynamic_density" << endl;
+    for (uint i = 0; i < density.size(); i++) {
+      out_file << i + 1 << "," << density[i].first << "," << density[i].second
+               << endl;
+    }
 
-  auto transformed_img_name = get_image_name(transformed_name, output_dir);
-  auto cropped_img_name = get_image_name(cropped_name, output_dir);
-
-  try {
-    validate_directory(output_dir);
-
-    imwrite(transformed_img_name, transformed_img);
-    cout << "[+] Transformed image saved to: " << transformed_img_name << endl;
-
-    imwrite(cropped_img_name, cropped_img);
-    cout << "[+] Cropped image saved to: " << cropped_img_name << endl;
-  } catch (const std::exception& e) {
-    cerr << "[-] Unable to save images" << endl;
-    std::cerr << e.what() << '\n';
+    out_file.close();
   }
 }
---
+++ assignment1/src/plotter.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("../output_files/density.csv")
+data = df.to_numpy()
+
+x = [d[0]/15 for d in data]
+y1 = [d[1] for d in data]
+y2 = [d[2] for d in data]
+
+plt.plot(x, y1, 'b', label="Queue Density")
+plt.plot(x, y2, 'r', label="Dynamic Density")
+plt.legend()
+plt.show()
+plt.savefig("../output_files/plot.png")
