3c1df7a29c7f17d9d01197852a379819991f48a2
Harsh Agrawal <harsh.ag14901@gmail.com>
2021-03-31 17:16:27 +0530

    Optimizations and feature additions for performance - Preprocess in worker for single threaded environment to avoid preprocessing when frame is to be skipped - Output parameter values along with runtime and error in separate CSV files - Reformat some files

--- assignment1/src/performance.cpp
+++ assignment1/src/performance.cpp
@@ -24,6 +24,35 @@ void perform_analysis(run_t f, int method) {
       throw runtime_error("Unsupported method : " + to_string(method));
   }
 }
+string get_test_param(runtime_params& test_params) {
+  return to_string(test_params.skip_frames) + ";" +
+         to_string(test_params.sparse_optical_flow) + ";" +
+         to_string(test_params.calc_dynamic_density) + ";" +
+         to_string(test_params.resolution.width) + "X" +
+         to_string(test_params.resolution.height) + ";" +
+         to_string(test_params.split_frame) + ";" +
+         to_string(test_params.split_video);
+}
+string get_test_param(runtime_params& baseline_params,
+                      runtime_params& test_params) {
+  if (baseline_params.skip_frames != test_params.skip_frames) {
+    return to_string(test_params.skip_frames);
+  } else if (baseline_params.sparse_optical_flow !=
+             test_params.sparse_optical_flow) {
+    return to_string(test_params.sparse_optical_flow);
+  } else if (baseline_params.calc_dynamic_density !=
+             test_params.calc_dynamic_density) {
+    return to_string(test_params.calc_dynamic_density);
+  } else if (baseline_params.resolution != test_params.resolution) {
+    return to_string(test_params.resolution.width) + "X" +
+           to_string(test_params.resolution.height);
+  } else if (baseline_params.split_frame != test_params.split_frame) {
+    return to_string(test_params.split_frame);
+  } else if (baseline_params.split_video != test_params.split_video) {
+    return to_string(test_params.split_video);
+  }
+  return "";
+}
 
 double compute_utility(density_t& baseline_density, density_t& test_density) {
   assert(baseline_density.size() == test_density.size());
@@ -48,20 +77,23 @@ double time_execution(run_t f, runtime_params& params, density_t& density) {
 }
 
 void analyze(run_t f, runtime_params& baseline_params,
-             vector<runtime_params>& test_params, string out_file) {
-  auto results = result_t();
+             vector<runtime_params>& test_params, string out_file,
+             bool verbose) {
+  result_t errorVtime = result_t(), paramVerror = result_t(),
+           paramVtime = result_t();
 
   auto baseline_density = density_t();
   auto baseline_time = time_execution(f, baseline_params, baseline_density);
   printf("[#] BASELINE TIME: %f\n", baseline_time);
-  results.push_back(make_pair(0, baseline_time));
+  errorVtime.push_back(make_pair(to_string(0), to_string(baseline_time)));
+  paramVerror.push_back(make_pair("", "0"));
+  paramVtime.push_back(make_pair("", to_string(baseline_time)));
 
   int i = 0;
 
   for (auto& test_param : test_params) {
-    
     i++;
-    cout << "Params for Trial: " << i << " are: " << endl;
+    cout << "[#] Params for Test " << i << " are: " << endl;
     test_param.print_params();
 
     auto test_density = density_t();
@@ -70,22 +102,28 @@ void analyze(run_t f, runtime_params& baseline_params,
 
     printf("[#] TEST TIME: %f\n", test_time);
 
-    results.push_back(make_pair(utility, test_time));
+    errorVtime.push_back(make_pair(to_string(utility), to_string(test_time)));
+    auto param = (verbose) ? get_test_param(test_param)
+                           : get_test_param(baseline_params, test_param);
+    paramVerror.push_back(make_pair(param, to_string(utility)));
+    paramVtime.push_back(make_pair(param, to_string(test_time)));
   }
 
-  outputCSV(out_file, results, "utility_error,time");
+  outputCSV(out_file + "_eVt.csv", errorVtime, "utility_error,time");
+  outputCSV(out_file + "_pVe.csv", paramVerror, "param,error");
+  outputCSV(out_file + "_pVt.csv", paramVtime, "param,time");
 }
 
 void method1(run_t f) {
   auto baseline_params = runtime_params{};
   vector<runtime_params> test_params;
 
-  for (int i = 3; i < 7; i += 2) {
+  for (int i = 1; i < 8; i++) {
     auto test_param = runtime_params{};
     test_param.skip_frames = i;
     test_params.push_back(test_param);
   }
-  analyze(f, baseline_params, test_params, "./output_files/skip_frames.csv");
+  analyze(f, baseline_params, test_params, "./output_files/skip_frames");
 }
 
 void method3(run_t f) {
@@ -107,7 +145,7 @@ void method3(run_t f) {
   // add_res(1280, 1024);
   add_res(1280, 720);
 
-  analyze(f, baseline_params, test_params, "./output_files/resolution.csv");
+  analyze(f, baseline_params, test_params, "./output_files/resolution");
 }
 
 void method4(run_t f) {
@@ -124,7 +162,7 @@ void method4(run_t f) {
   add_threads(6);
   add_threads(8);
 
-  analyze(f, baseline_params, test_params, "./output_files/split_video.csv");
+  analyze(f, baseline_params, test_params, "./output_files/split_video");
 }
 void method5(run_t f) {
   auto baseline_params = runtime_params{};
@@ -140,7 +178,7 @@ void method5(run_t f) {
   // add_threads(6);
   // add_threads(8);
 
-  analyze(f, baseline_params, test_params, "./output_files/split_frame.csv");
+  analyze(f, baseline_params, test_params, "./output_files/split_frame");
 }
 
 void method2(run_t f) {
@@ -152,31 +190,29 @@ void method2(run_t f) {
   test_params.push_back(baseline_params);
   test_params.back().sparse_optical_flow = true;
 
-  analyze(f, baseline_params, test_params, "./output_files/sparse_optical.csv");
+  analyze(f, baseline_params, test_params, "./output_files/sparse_optical");
 }
 
-void complete_analysis(run_t f){
-
+void complete_analysis(run_t f) {
   runtime_params baseline_params;
   vector<runtime_params> test_params;
 
+  int split_vids[] = {1, 4, 8};
+  int split_f[] = {1, 2, 4};
+  bool sparse[] = {false, true};
+  int resolutions[] = {1, 3, 5};
+  int skip_frame[] = {0, 1, 3};
 
-  int split_vids[] = {1,4,8};
-  int split_f[] = {1,2,4};
-  bool sparse[] = {false,true};
-  int resolutions[] = {1,3,5};
-  int skip_frame[] = {0,1,3};
-
-  for(int sv: split_vids){
-    for(int sf: split_f){
-      for(bool spa:sparse){
-        for (int res: resolutions){
-          for(int skf: skip_frame){
-            if(sv!=1 && sf!=1){
+  for (int sv : split_vids) {
+    for (int sf : split_f) {
+      for (bool spa : sparse) {
+        for (int res : resolutions) {
+          for (int skf : skip_frame) {
+            if (sv != 1 && sf != 1) {
               continue;
             }
             runtime_params rp;
-            rp.set_values(skf,spa,true,res,sf,sv);
+            rp.set_values(skf, spa, true, res, sf, sv);
             test_params.push_back(rp);
           }
         }
@@ -184,7 +220,6 @@ void complete_analysis(run_t f){
     }
   }
 
-  analyze(f, test_params[0], test_params, "./output_files/complete_analysis.csv");
-
-
+  analyze(f, test_params[0], test_params, "./output_files/complete_analysis",
+          true);
 }
--- assignment1/src/performance.hpp
+++ assignment1/src/performance.hpp
@@ -12,9 +12,13 @@ typedef void (*run_t)(runtime_params&, density_t&);
 double compute_utility(density_t& baseline_density, density_t& test_density);
 double time_execution(run_t f, runtime_params& params, density_t& density);
 void analyze(run_t f, runtime_params& baseline_params,
-             vector<runtime_params>& test_params, string out_file);
+             vector<runtime_params>& test_params, string out_file,
+             bool verbose = false);
 
 void perform_analysis(run_t f, int method);
+string get_test_param(runtime_params& baseline_params,
+                      runtime_params& test_params);
+string get_test_param(runtime_params& test_params);
 
 void method1(run_t f);
 void method2(run_t f);
